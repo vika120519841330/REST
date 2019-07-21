ed1497d3255d244c6f811610404e10d54e8d171a
Vika Strigo <vika120519841330@gmail.com>
2019-07-21 08:01:20 +0300

    add атрибуты маршрутизации
    add атрибуты маршрутизации
    контроллер bill по-прежнему не запускается

--- RESTfullAPI/App_Start/WebApiConfig.cs
+++ RESTfullAPI/App_Start/WebApiConfig.cs
@@ -9,11 +9,9 @@ namespace RESTfullAPI
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
-
             // Web API routes
             config.MapHttpAttributeRoutes();
-
+            // Web API configuration and services
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
--- RESTfullAPI/Controllers/BillController.cs
+++ RESTfullAPI/Controllers/BillController.cs
@@ -8,6 +8,7 @@ using RESTfullAPI.Models;
 
 namespace RESTfullAPI.Controllers
 {
+    [RoutePrefix("Bill")]
     public class BillController : ApiController
     {
         private IBillBase bl;
@@ -24,22 +25,26 @@ namespace RESTfullAPI.Controllers
         }
 
         [HttpGet]
+        [Route("{id:int}")]
         Bill GetBill(int id)
         {
             return bl.GetBill(id);
         }
 
         [HttpPost]
+        [Route("{inst:Bill}")]
         Bill OpenBill(Bill inst)
         {
             return bl.OpenBill(inst);
         }
         [HttpPut]
+        [Route("{inst:Bill}/{sum:int}")]
         Bill DebitCreditBill(Bill inst, int sum)
         {
             return bl.DebitCreditBill(inst, sum);
         }
         [HttpDelete]
+        [Route("{id:int}")]
         void CloseBill(int id)
         {
             bl.CloseBill(id);
